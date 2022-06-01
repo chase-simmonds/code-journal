9dd85363376f30d6889394207fab1103f0aa4920
Chase Simmonds <chasexsimmonds@gmail.com>
2022-06-01 19:00:02 +0000

    entries save to local storage

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousEntriesJSON = localStorage.getItem('code-journal-storage');
+if (previousEntriesJSON !== null) {
+  data.entries = JSON.parse(previousEntriesJSON);
+}
+
+function beforeUnload(event) {
+  var entriesJSON = JSON.stringify(data.entries);
+  localStorage.setItem('code-journal-storage', entriesJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
