1acefa9f25b9e31da1d15713f34f8f3245ba8eed
Chase Simmonds <chasexsimmonds@gmail.com>
2022-06-01 05:53:00 +0000

    listen for submit events, put form's values into new object, added and incremented nextentryID, prepend the new objects to the entries in the data model, reset img preview, reset form inputs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-var $entryImg = document.querySelector('img');
 var $photoUrl = document.querySelector('#photoUrl');
+var $entryImg = document.querySelector('#entry-img');
 
 function photoUpload(event) {
   $entryImg.setAttribute('src', event.target.value);
 }
 
 $photoUrl.addEventListener('input', photoUpload);
+
+var $entryForm = document.querySelector('#entry-form');
+var $entryTitle = document.querySelector('#title');
+var $entryNotes = document.querySelector('#notes');
+
+function userFormData(event) {
+  event.preventDefault();
+  var entryData = {
+    title: $entryTitle.value,
+    photoUrl: $photoUrl.value,
+    notes: $entryNotes.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(entryData);
+  $entryImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $entryForm.reset();
+}
+
+$entryForm.addEventListener('submit', userFormData);
